7b43c92a54803d007a87292b06d737e70772b547
Carl Anderson <nexusrpg@gmail.com>
2016-09-25 19:34:34 -0700

    Implemented UWP to get planets from Subsector data, and trade codes from a planet's UWP

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,4 +1,12 @@
 class Planet < ApplicationRecord
   #TODO: Should be imported from the Subsector creation automatically
   belongs_to :subsector, touch: true
+
+  def trade_codes
+    begin
+      return self.uwp.match(/^.+\s+\d{4}\s+\w\d{6}-\d\s+\w+(\D+)$/)[1].strip.titleize
+    rescue
+      return ''
+    end
+  end
 end
--- app/models/subsector.rb
+++ app/models/subsector.rb
@@ -5,5 +5,10 @@ class Subsector < ApplicationRecord
 
   def import_uwp
     #TODO: Iterate over data in uwp and extract trade codes, building the planets
+    uwp.split("\n").each do |line|
+      next unless line.match(/^.+\s+\d{4}\s+\w\d{6}-\d\s+\w+\D+$/)
+      name = line.match(/^(.+)\s+\d/)[1]
+      self.planets.create!(name: name.strip, uwp: line.strip)
+    end
   end
 end
